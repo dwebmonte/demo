8b522b86d52f4cca4312d57280f8b8382ee6b95e
satuevsky <satuevsky@gmail.com>
2018-10-30 23:04:49 +0300

    environment configuration commands

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+sudo yum install php-mysqli
+sudo yum install php-mbstring
+
+sudo yum install wget
+sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+sudo yum install mysql-server
+
+sudo systemctl start mysqld
+sudo systemctl start httpd
+
+sudo grep 'temporary password' /var/log/mysqld.log
+sudo mysql_secure_installation
+
+sudo mysql -u root -p < /var/www/html/install_sql.sql
+
+sudo chcon -t httpd_sys_content_t /var/www/html -R
+sudo chcon -t httpd_sys_rw_content_t /var/www/html -R

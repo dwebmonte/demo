629f762b95423d0a50b6e81f7e59a72958c022e9
satuevsky <satuevsky@gmail.com>
2018-12-26 22:41:46 +0300

    Configs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
+sudo cp /var/www/html/scripts/httpd-conf.d/* /etc/httpd/conf.d
+
 sudo yum install php-mysqli
 sudo yum install php-mbstring
 
@@ -13,7 +15,10 @@ sudo grep 'temporary password' /var/log/mysqld.log
 sudo mysql_secure_installation
 sudo mysql -u root -p < /var/www/html/install_sql.sql
 
+sudo chmod 777 -R /var/www/html
+sudo chown -R apache /var/www/html
 sudo chcon -t httpd_sys_content_t /var/www/html -R
 sudo chcon -t httpd_sys_rw_content_t /var/www/html -R
 
 sudo crontab /var/www/html/scripts/cron
+sudo systemctl restart crond
